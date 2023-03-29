0580a0d424ae08f2289846aad633d855e00b846e
Mateus Oliveira <mateus.oliveira@ccc.ifcg.edu.br>
2023-03-28 22:20:24 -0300

    implementando resumos

--- lab02/RegistroResumos.java
+++ lab02/RegistroResumos.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * escrever documentacao
  *
@@ -7,27 +9,54 @@
 public class RegistroResumos {
 
     private int numeroDeResumos;
+    private String[] titulos;
+    private String[] conteudo;
 
     public RegistroResumos(int numeroDeResumos){
+    	this.numeroDeResumos = numeroDeResumos;
+    	titulos = new String[this.numeroDeResumos];
+    	conteudo = new String[this.numeroDeResumos];
 
     }
 
+    private int contador;
     public void adiciona(String classes, String s) {
+    	contador++;
+    	for(int i = 0; i < titulos.length; i++) {
+    		titulos[i] = classes;
+    		conteudo[i] = s;
+    	}
     }
 
-    public String[] pegaResumos() {
-        return new String[0];
+	public String[] pegaResumos() {
+    	String[] resumos = new String[titulos.length];
+    	for(int i = 0; i < 1; i++) {
+    		String resumo = titulos[i] + ": " + conteudo[i];
+        	resumos[i] = resumo;	
+    	}
+        return resumos;
     }
 
-    public boolean imprimeResumos() {
-        return false;
+    public String imprimeResumos() {
+    	String saida = "- " + conta() + " resumo(s) cadastrado(s)\n";
+    	String resumos = "- ";
+    	for(String titulo: titulos) {
+    		resumos += titulo + " | ";
+    	}
+    	saida += resumos;
+        return saida;
     }
 
     public boolean temResumo(String classes) {
+    	for(String tituloResumo: titulos) {
+    		if(tituloResumo.equals(classes)) {
+    			return true;
+    		}
+    	}
         return false;
     }
 
     public int conta() {
-        return 0;
+        return contador;
     }
 }
