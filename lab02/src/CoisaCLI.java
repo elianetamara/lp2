51c6a03d4bf5da1008f1442d31d1d0e8385bfe63
Mateus Oliveira <mateus.oliveira@ccc.ifcg.edu.br>
2023-04-01 17:29:44 -0300

    Criando interação com o usuário

--- lab02/src/CoisaCLI.java
+++ lab02/src/CoisaCLI.java
@@ -1,31 +1,157 @@
 package src;
 
+import java.util.Scanner;
+
 public class CoisaCLI {
     public static void main(String[] args) {
-        System.out.println("-----");
+        Scanner sc = new Scanner(System.in);
+        carregaMenu(sc);
     }
 
-    String menuTempoOnline = "Registro de Tempo Online:\n" +
-            "- TEMPOONLINE HORAS <num>: adicionar <num> horas\n" +
-            "- TEMPOONLINE META: verificar se atingiu a meta de tempo";
+    static void carregaMenu(Scanner entrada){
+        System.out.println(opcoesMenu);
+        String atividade = entrada.nextLine();
+        switch (atividade) {
+            case OpcoesMenu.TEMPOONLINE:
+                menuTempoOnline(entrada);
+                break;
 
-    String menuDescanso = "lab02.Descanso:\n" +
-            "- DESCANSO HORAS <num>: definir horas de descanso\n" +
-            "- DESCANSO EMOJI <emoji>: definir emoji\n" +
-            "- DESCANSO SEMANAS <num>: define número de semanas\n" +
-            "- DESCANSO STATUS: ver status geral";
+            case OpcoesMenu.DESCANSO:
+                menuDescanso(entrada);
+                break;
 
-    String menuDisciplina = "lab02.Disciplina:\n" +
-            "- DISCIPLINA HORAS: cadastra horas de estudo\n" +
-            "- DISCIPLINA NOTA: cadastra notas\n" +
-            "- DISCIPLINA AROVADO: verifica se o aluno foi aprovado";
+            case OpcoesMenu.DISCIPLINA:
+                menuDisciplina(entrada);
+                break;
+
+            case OpcoesMenu.RESUMOS:
+                menuResumos(entrada);
+                break;
+
+            default:
+                //perguntar se o usuario deseja sair
+        }
+    }
+
+    private static void menuTempoOnline(Scanner entrada) {
+        System.out.println(opcoesMenuTempoOnline);
+        String comando = entrada.nextLine();
+        switch (comando) {
+            case OpcoesMenu.HORAS:
+                System.out.println("Digite quantas horas você quer adicionar:");
+                int horas = entrada.nextInt();
+                //adicionaTempoOnline(horas);
+                break;
+            case OpcoesMenu.META:
+                //atingiuMetaTempoOnline()
+                break;
+        }
+    }
+
+    private static void menuDescanso(Scanner entrada) {
+        System.out.println(opcoesMenuDescanso);
+        String comando = entrada.nextLine();
+        switch (comando) {
+            case OpcoesMenu.HORAS:
+                System.out.println("Digite quantas horas você quer adicionar:");
+                int horas = entrada.nextInt();
+                //comando
+                break;
+            case OpcoesMenu.EMOJI:
+                String emoji = entrada.nextLine();
+                //comando
+                break; 
+            case OpcoesMenu.SEMANAS:
+                int semanas = entrada.nextInt();
+                //comando
+                break;
+            case OpcoesMenu.STATUS:
+                //comando
+                break;
+        }
+    }
+
+    private static void menuDisciplina(Scanner entrada) {
+        System.out.println(opcoesMenuDisciplina);
+        String comando = entrada.nextLine();
+        switch (comando) {
+            case OpcoesMenu.HORAS:
+                System.out.println("Digite quantas horas você quer adicionar:");
+                int horas = entrada.nextInt();
+                //comando
+                break;
+            case OpcoesMenu.NOTA:
+                System.out.println("Digite qual nota e o valor dela:");
+                int numNota = entrada.nextInt();
+                double nota = entrada.nextDouble();
+                //comando
+                break;
+            case OpcoesMenu.APROVADO:
+                //comando
+                break;
+        }
+    }
+
+    private static void menuResumos(Scanner entrada) {
+        System.out.println(opcoesMenuResumos);
+        String comando = entrada.nextLine();
+        switch (comando) {
+            case OpcoesMenu.ADICIONA:
+                System.out.println("Digite o título e o conteúdo do resumo:");
+                String titulo = entrada.next();
+                String conteudo = entrada.next();
+                //comando
+                break;
+            case OpcoesMenu.PEGA:
+                //comando
+                break;
+            case OpcoesMenu.IMPRIME:
+                //comando
+                break;
+            case OpcoesMenu.TEM:
+                System.out.println("Digite o título do resumo:");
+                String tituloPesquisa = entrada.next();
+                //comando
+                break;
+            case OpcoesMenu.CONTA:
+                //comando
+                break;
+            case OpcoesMenu.BUSCA:
+                System.out.println("Digite a palavra a ser procurada:");
+                String palavra = entrada.next();
+                //comando
+                break;
+        }
+    }
+
+    static String opcoesMenu = "Bem vindo ao coISA! Selecione uma das atividades abaixo:\n" +
+            "- Registro de Tempo Online -> TEMPOONLINE\n" +
+            "- Descanso -> DESCANSO\n" +
+            "- Disciplina -> DISCIPLINA\n" +
+            "- Registro de Resumos -> RESUMOS";
 
 
-    String menuResumos = "Registro de Resumos:\n" +
-            "- RESUMOS ADICIONA <titulo> <conteudo>: adiciona resumo\n" +
-            "- RESUMOS PEGA: pega lista de resumos\n" +
-            "- RESUMOS IMPRIME: informações sobre os resumos\n" +
-            "- RESUMOS TEM <titulo>: procura resumo sobre determinado tema\n" +
-            "- RESUMOS CONTA: informa quantos resumos foram cadastrados\n" +
-            "- RESUMOS BUSCA <texto>:";
+    static String opcoesMenuTempoOnline = "Registro de Tempo Online:\n" +
+            "- HORAS -> adicionar <num> horas\n" +
+            "- META -> verificar se atingiu a meta de tempo";
+
+    static String opcoesMenuDescanso = "Descanso:\n" +
+            "- HORAS -> definir horas de descanso\n" +
+            "- EMOJI -> definir emoji\n" +
+            "- SEMANAS -> define número de semanas\n" +
+            "- STATUS -> ver status geral";
+    
+    static String opcoesMenuDisciplina = "Disciplina:\n" +
+            "- HORAS -> cadastra horas de estudo\n" +
+            "- NOTA -> cadastra notas\n" +
+            "- APROVADO -> verifica se o aluno foi aprovado";
+
+
+    static String opcoesMenuResumos = "Registro de Resumos:\n" +
+            "- ADICIONA -> adiciona resumo\n" +
+            "- PEGA -> pega lista de resumos\n" +
+            "- IMPRIME -> informações sobre os resumos\n" +
+            "- TEM -> procura resumo sobre determinado tema\n" +
+            "- CONTA -> informa quantos resumos foram cadastrados\n" +
+            "- BUSCA -> procura resumos com determinada palavra";
 }
---
+++ lab02/src/OpcoesMenu.java
@@ -0,0 +1,23 @@
+package src;
+
+public enum OpcoesMenu {
+
+    CONSTANTE;
+    public static final String TEMPOONLINE = "TEMPOONLINE";
+    public static final String DESCANSO = "DESCANSO";
+    public static final String DISCIPLINA = "DISCIPLINA";
+    public static final String RESUMOS = "RESUMOS";
+    public static final String HORAS = "HORAS";
+    public static final String META = "META";
+    public static final String EMOJI = "EMOJI";
+    public static final String SEMANAS = "SEMANAS";
+    public static final String STATUS = "STATUS";
+    public static final String NOTA = "NOTA";
+    public static final String APROVADO = "APROVADO";
+    public static final String ADICIONA = "ADICIONA";
+    public static final String PEGA = "PEGA";
+    public static final String IMPRIME = "IMPRIME";
+    public static final String TEM = "TEM";
+    public static final String CONTA = "CONTA";
+    public static final String BUSCA = "BUSCA";
+}
