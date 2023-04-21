7dc09f0c1f0fd3a3c9e3de55e1c916f2e01d1ba9
Mateus Oliveira <mateus.oliveira@ccc.ifcg.edu.br>
2023-04-21 13:35:30 -0300

    Teste de contato

--- lab03/src/agenda/Contato.java
+++ lab03/src/agenda/Contato.java
@@ -9,44 +9,26 @@ package agenda;
 
 public class Contato {
 
-    private int posicao;
     private String nome;
     private String sobrenome;
     private String telefone;
 
-    public Contato(){
-        nome = "";
-        sobrenome = "";
-        telefone = "";
-        posicao = 0;
+    public Contato(String nome, String sobrenome, String telefone){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.telefone = telefone;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getSobrenome() {
         return sobrenome;
     }
 
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
     public String getTelefone() {
         return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public void setPosicao(int posicao) {
-        this.posicao = posicao;
     }
 
     /**
--- lab03/src/tests/ContatoTest.java
+++ lab03/src/tests/ContatoTest.java
@@ -9,29 +9,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ContatoTest {
 
-    private Contato contatoBase;
+    private Contato contatoTeste;
 
-   /* @BeforeEach
-    void prepararContato() {
-        this.contatoBase = new Contato("Winicius", "Allan", "8399630-3246");
+    @BeforeEach
+    void criaContato() {
+        this.contatoTeste = new Contato("Eliane", "Tamara", "839876-1234");
     }
 
     @Test
     void testEquals() {
-        String msg = "Esperando comparar os contatos";
-        assertEquals(contatoBase, new Contato("Winicius", "Allan", "839984567"));
-        assertEquals(contatoBase, new Contato("   Winicius    ", "Allan", "839984567"));
-        assertNotEquals(contatoBase, new Contato("Eliane", "Araújo", "83 21012101"));
+        assertFalse(contatoTeste.equals("Matheus", "Gaudencio"));
+        assertTrue(contatoTeste.equals("Eliane", "Tamara"));
     }
 
     @Test
-    void testNomeNull() {
-        assertThrows(NullPointerException.class,
-                () -> new Contato(null, "Gaudêncio", "21010000")); // Nome null
-        assertThrows(NullPointerException.class,
-                () -> new Contato("Matheus", null, "21010000")); // Sobrenome null
-        assertThrows(NullPointerException.class,
-                () -> new Contato("Matheus", "Gaudêncio", null)); // Número null
-    }*/
+    void testaToString() {
+        assertEquals("Eliane Tamara\n839876-1234", contatoTeste.toString());
+    }
+
 }
 
