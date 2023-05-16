78cf4f462ba66273d1c9ccb081eadd1d1a76cea4
elianetamara <tamaraelianeee@gmail.com>
2023-05-16 09:27:14 -0300

    começando çab05

---
+++ lab05/src/main/java/Elemento.java
@@ -0,0 +1,25 @@
+public class Elemento {
+
+    private int prioridade;
+
+    private String valor;
+
+    public Elemento() {
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+}
---
+++ lab05/src/main/java/Titulo.java
@@ -0,0 +1,11 @@
+public class Titulo extends  Elemento{
+
+    private int nivel;
+
+    private boolean linkavel;
+
+    public Titulo(int nivel, boolean linkavel) {
+        this.nivel = nivel;
+        this.linkavel = linkavel;
+    }
+}
